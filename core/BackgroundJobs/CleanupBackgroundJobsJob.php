9dbc57d1ef0e42466ddead72b7bacecaa123c317
Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>
2026-06-04 20:30:47 +0200

    feat(jobs): clean old job runs
    Signed-off-by: Benjamin Gaussorgues <benjamin.gaussorgues@nextcloud.com>

--- core/BackgroundJobs/CleanupBackgroundJobsJob.php
+++ core/BackgroundJobs/CleanupBackgroundJobsJob.php
@@ -15,15 +15,17 @@ use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\BackgroundJob\IJob;
 use OCP\BackgroundJob\JobStatus;
 use OCP\BackgroundJob\TimedJob;
-use OCP\IServerInfo;
+use OCP\IConfig;
 use Override;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 
 class CleanupBackgroundJobsJob extends TimedJob {
 	public function __construct(
 		ITimeFactory $time,
 		private readonly JobRuns $jobRuns,
 		private readonly IServerInfo $serverInfo,
+		private readonly IConfig $config,
 		private readonly LoggerInterface $logger,
 	) {
 		parent::__construct($time);
@@ -34,8 +36,7 @@ class CleanupBackgroundJobsJob extends TimedJob {
 	#[Override]
 	protected function run($argument): void {
 		$this->reapCrashedJobs();
-
-		// TODO Clean oldest jobs
+		$this->cleanOldestRuns();
 	}
 
 	private function reapCrashedJobs(): void {
@@ -64,6 +65,21 @@ class CleanupBackgroundJobsJob extends TimedJob {
 					'runId' => $job->runId,
 				]);
 			}
+		}
+	}
+
+	private function cleanOldestRuns(): void {
+		$daysToKeep = $this->config->getSystemValueInt('background_jobs_expiration_days', 60);
+		if ($daysToKeep < 1) {
+			throw new RuntimeException('Invalid number of days');
+		}
+		$cleanBeforeTimestamp = time() - ($daysToKeep * 24 * 3600);
+
+		$cleanedJobs = $this->jobRuns->deleteBefore($cleanBeforeTimestamp);
+		if ($cleanedJobs > 0) {
+			$this->logger->info(
+				'Cleanup of old background jobs. Number of jobs removed: ' . $cleanedJobs . 'Reason: older than ' . $daysToKeep . ' days.',
+			);
 		}
 	}
 }
